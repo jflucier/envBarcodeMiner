8434e446c55444c8ed696d95e1ee8f4ad28f9494
jflucier <3505568+jflucier@users.noreply.github.com>
2025-03-25 14:42:10 -0400

    multiple dev

--- install_db.sh
+++ install_db.sh
@@ -10,6 +10,8 @@ help_message () {
 	echo ""
 	echo "	-db STR	path to database dir (default to envBarcodeMiner_installation_path/db)"
   echo "	-c BOOL	Specify if download core_nt files downloaded from NCBI should be deleted (defaults to false)"
+  echo "	-t INT	Maximum number of threads to use (defaults 4)"
+
   echo ""
   echo "  -h --help	Display help"
 
@@ -17,14 +19,17 @@ help_message () {
 }
 
 export ENVBARCODEMINER_PATH=$(dirname "$0")
+export CONTAINER="${ENVBARCODEMINER_PATH}/containers/envBarcodeMiner.sif"
+
 cd ${ENVBARCODEMINER_PATH}
 
 # initialisation
 db="false"
 clean="false"
+threads="4"
 
 # load in params
-SHORT_OPTS="ht:db:c:"
+SHORT_OPTS="ht:db:c:t:"
 LONG_OPTS='help'
 
 OPTS=$(getopt -o $SHORT_OPTS --long $LONG_OPTS -- "$@")
@@ -42,41 +47,196 @@ while true; do
 		-h | --help) help_message; exit 1; shift 1;;
     -db) db=$2; shift 2;;
     -c) clean=$2; shift 2;;
+    -t) threads=$2; shift 2;;
     --) help_message; exit 1; shift; break ;;
 		*) break;;
 	esac
 done
 
 if [ "$db" = "false" ]; then
-    db=${ENVBARCODEMINER_PATH}/db
+  db=${ENVBARCODEMINER_PATH}/db
 fi
 
+if [[ "$threads" =~ ^[0-9]+$ ]]; then
+  echo "Will use ${threads} threads"
+else
+  echo "$threads is not a valid integer integer."
+  help_message; exit 1;
+fi
+
+echo "### Database will be installed in this path: ${db} ###"
 mkdir -p ${db}
 
-echo "Downloading core_nt db from NCBI (this might take a while...). Requires 500GB of disk space."
-python download_envBarcodeMiner_db.py db=${db}
+echo "### Downloading NCBI taxonomy ###"
+mkdir -p ${db}/taxonomy
+cd ${db}/taxonomy
+wget ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump.tar.gz
+tar -zxvf taxdump.tar.gz
+wget https://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/nucl_gb.accession2taxid.gz
+wget https://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/nucl_wgs.accession2taxid.EXTRA.gz
+wget https://ftp.ncbi.nih.gov/pub/taxonomy/accession2taxid/nucl_wgs.accession2taxid.gz
+gunzip nucl_gb.accession2taxid.gz
+gunzip nucl_wgs.accession2taxid.EXTRA.gz
+gunzip nucl_wgs.accession2taxid.gz
 
-echo "Generating envBarcodeMiner.core_nt.fa (this might take a while... again!). Requires another 1TB of disk space"
+cd ${ENVBARCODEMINER_PATH}
+echo "### Setting up taxonomy database ###"
 singularity exec --writable-tmpfs -e \
 -B ${ENVBARCODEMINER_PATH}:${ENVBARCODEMINER_PATH} \
-${ENVBARCODEMINER_PATH}/containers/envBarcodeMiner.sif \
-blastdbcmd -entry all -db ${ENVBARCODEMINER_PATH}/db/core_nt -out ${ENVBARCODEMINER_PATH}/db/envBarcodeMiner.core_nt.fa
+${CONTAINER} \
+perl /opt/taxdb/scripts/taxdb_create.pl ${db}/envBarcodeMiner_db.sqlite
 
-echo "Splitting up envBarcodeMiner.core_nt.fa into parts"
-mkdir -p ${db}/fa_split
+echo "### Importing taxonomy to db ###"
 singularity exec --writable-tmpfs -e \
 -B ${ENVBARCODEMINER_PATH}:${ENVBARCODEMINER_PATH} \
-${ENVBARCODEMINER_PATH}/containers/envBarcodeMiner.sif \
-seqkit split --threads 8 --by-size 125000 \
---out-dir ${ENVBARCODEMINER_PATH}/db/fa_split \
-${ENVBARCODEMINER_PATH}/db/envBarcodeMiner.core_nt.fa
+${CONTAINER} \
+perl /opt/taxdb/scripts/taxdb_add.pl ${db}/taxonomy_db.sqlite ${db}/taxonomy
 
-TOTAL_FA=$(ls ${ENVBARCODEMINER_PATH}/db/fa_split/* | wc -l)
-echo "Generated a total of ${TOTAL_FA} fasta for dicey search"
+sqlite3 ${db}/taxonomy_db.sqlite '.separator "\t"' '.header on' ".import ${db}/taxonomy/nucl_gb.accession2taxid accession2taxid"
+sqlite3 ${db}/taxonomy_db.sqlite '.separator "\t"' '.header on' ".import ${db}/taxonomy/nucl_wgs.accession2taxid accession2taxid"
+sqlite3 ${db}/taxonomy_db.sqlite '.separator "\t"' '.header on' ".import ${db}/taxonomy/nucl_wgs.accession2taxid.EXTRA accession2taxid"
+
+sqlite3 ${db}/taxonomy_db.sqlite "
+create index NAME_taxid_idx on NAME(tax_id);
+create index NODE_taxid_idx on NODE(tax_id);
+create index accession2taxid_accession_idx on accession2taxid(accession);
+create index accession2taxid_taxid_idx on accession2taxid(taxid);
+create index accession2taxid_accessionversion_idx on accession2taxid(accession.version);
+"
+
+echo "### Done setting up taxonomy db ###"
+
+echo "### Downloading core_nt db from NCBI (this might take a while... only 1 thread supported). Requires 500GB of disk space. ###"
+mkdir -p ${db}/NCBI
+python download_envBarcodeMiner_db.py db=${db}/NCBI
+
+echo "### Generating envBarcodeMiner.core_nt.fa (this might take a while... again single thread only!). Requires another 1TB of disk space ###"
+singularity exec --writable-tmpfs -e \
+-B ${ENVBARCODEMINER_PATH}:${ENVBARCODEMINER_PATH} \
+${CONTAINER} \
+blastdbcmd -entry all -db ${db}/NCBI/core_nt -out ${db}/envBarcodeMiner.core_nt.fa
+
+echo "### Splitting up envBarcodeMiner.core_nt.fa into parts ###"
+FA_SPLIT_DIR="${db}/split"
+mkdir -p ${FA_SPLIT_DIR}
+singularity exec --writable-tmpfs -e \
+-B ${ENVBARCODEMINER_PATH}:${ENVBARCODEMINER_PATH} \
+${CONTAINER} \
+seqkit split --threads ${threads} --by-size 125000 \
+--out-dir ${FA_SPLIT_DIR} \
+${db}/envBarcodeMiner.core_nt.fa
+
+fasta_files=$(ls ${FA_SPLIT_DIR}/*.fa)
+TOTAL_FA=$(ls ${fasta_files} | wc -l)
+echo "### Generated a total of ${TOTAL_FA} fasta to index ###"
 
 if [ "$clean" != "false" ]; then
-  echo "cleaning up downloaded NCBI files"
-  rm ${db}/*.tar.gz* ${db}/core_nt*
+  echo "### cleaning up downloaded NCBI files since they are not needed anymore ###"
+  rm -r ${db}/NCBI
 fi
 
-echo "DB installation done!"
+# --- Function to process each FASTA file ---
+process_fasta() {
+  local fa_path="$1"
+  local fm9_file="${fa_path}.fm9"
+  local thread_id="$2"
+
+  if [[ ! -e "${fm9_file}" ]]; then
+
+    # Define the singularity command with explicit PATH
+    local singularity_cmd="singularity exec --writable-tmpfs -e \
+      -B \"${FA_SPLIT_DIR}:${FA_SPLIT_DIR}\" \
+      --env \"PATH=/opt/dicey/bin:/usr/bin:$PATH\" \
+      \"${CONTAINER}\""
+
+    echo "### Indexing ${fa_path} ###"
+
+    echo "Thread $thread_id: zipping genome with bgzip for ${fa_path}"
+    eval "${singularity_cmd} bgzip --force --threads 1 \"${fa_path}\""
+
+    echo "Thread $thread_id: indexing genome with dicey for ${fa_path}"
+    eval "${singularity_cmd} dicey index -o \"${fa_path}.fm9\" \"${fa_path}.gz\""
+
+    echo "Thread $thread_id: indexing genome with samtools for ${fa_path}"
+    eval "${singularity_cmd} samtools faidx \"${fa_path}.gz\""
+  else
+    echo "Thread $thread_id: File ${fm9_file} already exists. Skipping."
+  fi
+}
+
+echo "### Indexing ${TOTAL_FA} fasta... ###"
+counter=0
+for fa_path in $fasta_files; do
+  counter=$((counter + 1))
+  process_fasta "$fa_path" "$counter" &
+  active_processes=$(jobs -r -p | wc -l)
+  while [[ "$active_processes" -ge "${threads}" ]]; do
+    sleep 0.1
+    active_processes=$(jobs -r -p | wc -l)
+  done
+done
+
+# --- Wait for all background processes to finish ---
+wait
+
+# check if fm9 files = total fa to see if errors
+TOTAL_INDEX=$(ls ${db}/fa_split/*.fa.fm9 | wc -l)
+if [[ "$TOTAL_INDEX" != "$TOTAL_FA" ]]; then
+  echo "### Detected some error in dicey index generation. Will try to recuperate. ###"
+  fasta_files=$(ls ${FA_SPLIT_DIR}/*.fa.gz)
+  for fa_path in $fasta_files; do
+    fa_path=${fa_path%.gz}
+    if [[ ! -e "${fa_path}.fm9" ]]; then
+        echo "A problem was detected with $fm9_file."
+        echo "will regenerate"
+        process_fasta "$fa_path" "$counter"
+    fi
+  done
+fi
+
+TOTAL_INDEX=$(ls ${db}/fa_split/*.fa.fm9 | wc -l)
+if [[ "$TOTAL_INDEX" != "$TOTAL_FA" ]]; then
+  echo "### Still detected some error in dicey index generation. Please investigate these files: ###"
+  fasta_files=$(ls ${FA_SPLIT_DIR}/*.fa.gz)
+  for fa_path in $fasta_files; do
+    fa_path=${fa_path%.gz}
+    if [[ ! -e "${fa_path}.fm9" ]]; then
+        echo "${fa_path}"
+    fi
+  done
+else
+  echo "### DB installation done sucessfully! ###"
+fi
+
+
+## OBSOLETE: single threaded version
+#for ((i=1; i<=TOTAL_FA; i++)); do
+#  fa_path=$(ls ${db}/fa_split/*.fa | awk "NR==${i}")
+#  fm9_file="${fa_path}.fm9"
+#  if [[ ! -e "${fm9_file}" ]]; then
+#    echo "### Indexing ${fa_path} (${i} / ${TOTAL_FA}) ###"
+#      echo "zipping genome with bgzip for ${fa_path}"
+#      singularity exec --writable-tmpfs -e \
+#      -B ${db}/fa_split:${db}/fa_split \
+#      ${ENVBARCODEMINER_PATH}/containers/envBarcodeMiner.sif \
+#      bgzip --force --threads ${threads} ${fa_path}
+#
+#      echo "indexing genome with dicey for ${fa_path}"
+#      singularity exec --writable-tmpfs -e \
+#      -H ${db}/fa_split \
+#      -B ${db}/fa_split:${db}/fa_split \
+#      ${ENVBARCODEMINER_PATH}/containers/envBarcodeMiner.sif \
+#      dicey index -o ${fa_path}.fm9 ${fa_path}.gz
+#
+#      echo "indexing genome with samtools for ${fa_path}"
+#      singularity exec --writable-tmpfs -e \
+#      -B ${db}/fa_split:${db}/fa_split \
+#      ${ENVBARCODEMINER_PATH}/containers/envBarcodeMiner.sif \
+#      samtools faidx ${fa_path}.gz
+#  else
+#    echo "File ${fm9_file} already exists. Skipping bgzip and dicey index."
+#  fi
+#done
+
+
+
--- run_dicey.local.sh
+++ run_dicey.local.sh
@@ -8,7 +8,7 @@ help_message () {
 	echo "Options:"
 
 	echo ""
-	echo "	-fa_list STR	Path to Fasta list to run dicey amplification against (defaults to envBarcodeMiner_install/db/fa_split)"
+  echo "	-db STR	STR	path to database dir (default to envBarcodeMiner_installation_path/db)"
 	echo "	-t	# of threads (default 12)"
 	echo "	-primer_f	Forward primer sequence to use as input for dicey"
 	echo "	-primer_r	Reverse primer sequence to use as input for dicey"
@@ -20,18 +20,18 @@ help_message () {
 	echo "";
 }
 
-export EXE_PATH=$(dirname "$0")
+export ENVBARCODEMINER_PATH=$(dirname "$0")
 
 # initialisation
+db="false"
 threads="12"
-fa_list="false"
 out="false"
 tmp="false"
 primer_f="false"
 primer_r="false"
 
 # load in params
-SHORT_OPTS="h:t:fa_list:o:tmp:primer_f:primer_r:"
+SHORT_OPTS="h:t:o:tmp:primer_f:primer_r:db:"
 LONG_OPTS='help'
 
 OPTS=$(getopt -o $SHORT_OPTS --long $LONG_OPTS -- "$@")
@@ -50,9 +50,9 @@ while true; do
         -t) threads=$2; shift 2;;
         -tmp) tmp=$2; shift 2;;
         -o) out=$2; shift 2;;
-		    -fa_list) fa_list=$2; shift 2;;
         -primer_f) primer_f=$2; shift 2;;
         -primer_r) primer_r=$2; shift 2;;
+        -db) db=$2; shift 2;;
         --) help_message; exit 1; shift; break ;;
 		*) break;;
 	esac
@@ -66,14 +66,14 @@ else
     echo "## Results wil be stored to this path: $out/"
 fi
 
+if [ "$db" = "false" ]; then
+  db=${ENVBARCODEMINER_PATH}/db
+fi
+
 if [ "$tmp" = "false" ]; then
     tmp=$out/temp
     mkdir -p $tmp
     echo "## No temp folder provided. Will use: $tmp"
-fi
-
-if [ "$fa_list" = "false" ]; then
-      fa_list=${EXE_PATH}/db/fa_split
 fi
 
 if [ "$primer_f" = "false" ]; then
@@ -86,18 +86,20 @@ if [ "$primer_r" = "false" ]; then
     help_message; exit 1
 fi
 
-echo "## Fasta path: $fa_list"
+fa_list=${db}/split
+
+echo "## Database path: $db"
 echo "## Forward primer sequence: $primer_f"
 echo "## Reverse primer sequence: $primer_r"
 echo "## Will run using ${threads} threads"
 
-echo "generate primer definition file im temp folder"
+echo "generate primer fasta im temp folder"
 echo ">primer_f
 $primer_f
 >primer_r
 $primer_r" > ${tmp}/primers.fa
 
-export DICEY_SIF=${EXE_PATH}/containers/envBarcodeMiner.sif
+export DICEY_SIF=${ENVBARCODEMINER_PATH}/containers/envBarcodeMiner.sif
 
 echo "copying primer3 configs from container to ${tmp}"
 singularity exec --writable-tmpfs -e \
@@ -105,36 +107,19 @@ singularity exec --writable-tmpfs -e \
 ${DICEY_SIF} \
 cp -r /opt/dicey/src/primer3_config ${tmp}/
 
-job_nbr=$(ls ${fa_list}/* | wc -l)
+job_nbr=$(ls ${fa_list}/*.fm9 | wc -l)
 
 for ((i=1; i<=job_nbr; i++)); do
-  echo "running ${fa_path} (${i} / ${job_nbr})"
+  echo "### running ${fa_path} (${i} / ${job_nbr}) ###"
 
-  fa_path=$(ls ${fa_list}/* | awk "NR==${i}")
-  echo "copying fasta ${fa_path} to temp folder"
-  cp "${fa_path}" "${tmp}/"
+  index_path=$(ls ${fa_list}/*.fm9 | awk "NR==${i}")
+  fa_path=${index_path%.fm9}
+  echo "copying dicey index ${fa_path} to temp folder"
+  cp "${fa_path}*" "${tmp}/"
 
   FA=$(basename ${fa_path})
-  echo "zipping genome with bgzip for $FA"
-  singularity exec --writable-tmpfs -e \
-  -B ${tmp}:${tmp} \
-  ${DICEY_SIF} \
-  bgzip --force --threads ${threads} ${tmp}/${FA}
 
-  echo "indexing genome with dicey for $FA"
-  singularity exec --writable-tmpfs -e \
-  -H ${tmp} \
-  -B ${tmp}:${tmp} \
-  ${DICEY_SIF} \
-  dicey index -o ${tmp}/${FA}.fm9 ${tmp}/${FA}.gz
-
-  echo "indexing genome with samtools for $FA"
-  singularity exec --writable-tmpfs -e \
-  -B ${tmp}:${tmp} \
-  ${DICEY_SIF} \
-  samtools faidx ${tmp}/${FA}.gz
-
-  echo "running dicey search for $FA"
+  echo "running dicey search on ${FA}"
   singularity exec --writable-tmpfs -e \
   -H ${tmp} \
   -B ${tmp}:${tmp} \
@@ -154,13 +139,217 @@ for ((i=1; i<=job_nbr; i++)); do
   -m amplicon \
   -j ${tmp}/${FA}.json.gz > ${tmp}/${FA}.tsv
 
-  echo "copying results to ${OUTPATH}"
-  mkdir -p ${OUTPATH}
-  cp ${tmp}/${FA}.tsv ${OUTPATH}/
-  cp ${tmp}/${FA}.json.gz ${OUTPATH}/
+  echo "copying results to ${out}"
+  mkdir -p ${out}/dicey/part/
+  cp ${tmp}/${FA}.tsv ${out}/dicey/part/
+  cp ${tmp}/${FA}.json.gz ${out}/dicey/part/
 
   echo "cleaning up temp"
   rm ${tmp}/${FA}*
 done
 
+echo "combine all dicey results in ${out}/dicey_results.tsv"
+cat ${out}/dicey/part/*.tsv > ${out}/dicey/dicey_results.tsv
+
+echo "trim hits accession version from all hits"
+perl -ne '
+chomp($_);
+if($_ =~ /^Amplicon\tId/){
+  print $_ . "\n";
+}
+else{
+  my @t = split("\t",$_);
+  my $tmp = $t[4];
+  my($ns) = $t[4] =~ /^(.*)\.\d+$/;
+  $t[4] = $ns;
+  print join("\t",@t) . "\n";
+}
+' ${out}/dicey/dicey_results.tsv > ${out}/dicey/dicey_results.acc.tsv
+
+echo "Initialise envBarcodeMiner result db with dicey hits"
+sqlite3 ${out}/envBarcodeMiner.results.sqlite "
+drop table hits;
+create table hits (
+Amplicon TEXT,
+Id INTEGER,
+Length INTEGER,
+Penalty FLOAT,
+Chrom TEXT,
+ForPos INTEGER,
+ForEnd INTEGER,
+ForTm FLOAT,
+ForName TEXT,
+ForSeq TEXT,
+ChromV TEXT,
+RevPos INTEGER,
+RevEnd INTEGER,
+RevTm FLOAT,
+RevName TEXT,
+RevSeq TEXT,
+Seq TEXT
+);
+"
+sqlite3 ${out}/envBarcodeMiner.results.sqlite '.separator "\t"' '.header on' ".import ${out}/dicey/dicey_results.acc.tsv hits"
+
+sqlite3 ${out}/envBarcodeMiner.results.sqlite "
+delete from trnL_hits where Chrom='Chrom';
+"
+
+echo "associate taxonomic information to dicey hits"
+sqlite3 ${out}/envBarcodeMiner.results.sqlite "
+ATTACH DATABASE ${db}/taxonomy_db.sqlite AS taxo;
+drop table hits_taxid;
+create table hits_taxid as
+select
+  h.Chrom || \"_\" || h.Id Id,
+  h.Chrom,
+  a.taxid,
+  h.Seq
+from
+  hits h
+  join taxo.accession2taxid a on h.Chrom=a.\"accession\";
+"
+
+echo "Generate dicey hits TSV report: hits.taxid.tsv"
+sqlite3 ${out}/envBarcodeMiner.results.sqlite '.separator "\t"' '.header off' '
+select distinct
+   taxid,
+   Chrom,
+   Seq
+from trnL_hits_taxid
+order by taxid
+' > ${out}/dicey/hits.taxid.tsv
+
+echo "Generate dicey hits lineage TSV report: hits.lineage.tsv"
+echo -e "taxid\tacc\tlineage\tseq" > ${out}/dicey/hits.lineage.tsv
+tmp_taxid=""
+while IFS=$'\t' read -r taxid acc seq ; do
+  if [ "$taxid" = "$tmp_taxid" ]; then
+    echo "$taxid == $tmp_taxid --> ID=${tmp_id}"
+    echo -e "$taxid\t$acc\t${lineage}_${tmp_id}\t$seq" >> ${out}/dicey/hits.lineage.tsv
+  else
+    tmp_id=1
+    echo "$taxid != $tmp_taxid --> ID=${tmp_id}"
+    tmp=$(
+      for t in $(singularity exec --writable-tmpfs -e \
+        -H ${tmp} \
+        -B ${tmp}:${tmp} \
+        ${DICEY_SIF} \
+        perl /opt/taxdb/scripts/taxdb_query.pl --taxon $taxid --mode lineage ${out}/dicey/envBarcodeMiner.results.sqlite);
+      do
+        singularity exec --writable-tmpfs -e \
+        -H ${tmp} \
+        -B ${tmp}:${tmp} \
+        ${DICEY_SIF} \
+        perl /opt/taxdb/scripts/taxdb_query.pl --taxon ${t} ${out}/dicey/envBarcodeMiner.results.sqlite | grep -m 1 "scientific name";
+      done | \
+        cut -f 1-3,15,16 | \
+        grep -P 'kingdom|phylum|class|order|family|genus|species' | \
+        grep -vP '\tsuper\w+' | \
+        grep -vP '\tsub\w+' | \
+        grep -vP '\tno rank' | \
+        sed "s/'/ /g" | cut -f 4 | perl -ne 'chomp($_); print $_ . ","'
+    )
+
+    lineage=$(
+    perl -e "
+    my \$tt = '$tmp';
+    my @t = split(',',\$tt);
+    my @rv = reverse(@t);
+    my \$s = join(';',@rv);
+    print \$s. \";$acc\" . \"\n\";
+    "
+    )
+    echo -e "$taxid\t$acc\t${lineage}_${tmp_id}\t$seq" >> ${out}/dicey/hits.lineage.tsv
+  fi
+
+  tmp_taxid=$taxid
+  tmp_id=$((tmp_id+1))
+done < ${out}/dicey/hits.taxid.tsv
+
+# split header and reimport to db
+perl -ne '
+chomp($_);
+my($taxid,$acc,$lineage,$seq) = split("\t",$_);
+my($kingdom,$phylum,$class,$order,$family,$genus,$species,$lacc) = split("\;",$lineage);
+print "$taxid\t$acc\t$kingdom\t$phylum\t$class\t$order\t$family\t$genus\t$species\t$lacc\t$lineage\t$seq\n";
+' ${out}/dicey/hits.lineage.tsv > ${out}/dicey/hits.lineage.split.tsv
+
+sqlite3 ${out}/envBarcodeMiner.results.sqlite "
+drop table hits_lineage;
+create table hits_lineage (
+  taxid integer,
+  accession text,
+  kingdom text,
+  phylum text,
+  class text,
+  t_order text,
+  family text,
+  genus text,
+  species text,
+  t_accesion text,
+  lineage text,
+  seq text
+);
+"
+sqlite3 ${out}/envBarcodeMiner.results.sqlite '.separator "\t"' '.header on' ".import ${out}/dicey/hits.lineage.split.tsv hits_lineage"
+
+sqlite3 ${out}/envBarcodeMiner.results.sqlite '.separator "\t"' '.header off' '
+select
+   Seq,
+   group_concat(distinct kingdom),
+   group_concat(distinct phylum),
+   group_concat(distinct class),
+   group_concat(distinct t_order),
+   group_concat(distinct family),
+   group_concat(distinct genus),
+   group_concat(distinct species),
+   group_concat(distinct t_accesion)
+from trnL_hits_lineage
+group by Seq
+' > ${out}/hits.lineage.byseq.tsv
+
+# produce fasta
+perl -ne '
+chomp($_);
+my($seq,@t) = split("\t",$_);
+if(length($seq) >= 20){
+  my $h = "";
+  foreach my $tc (@t){
+    my @tmp = split(",",$tc);
+    if(scalar(@tmp) > 1){
+      break;
+    }
+    else{
+      $h .= "$tc\;";
+    }
+  }
+  chop($h);
+  print "\>$h\n$seq\n";
+}
+' ${out}/hits.lineage.byseq.tsv > ${out}/hits.lineage.fa
+
+perl -ne '
+chomp($_);
+my($seq,@t) = split("\t",$_);
+pop(@t);
+if(length($seq) >= 20){
+  my $h = "";
+  foreach my $tc (@t){
+    my @tmp = split(",",$tc);
+    if(scalar(@tmp) > 1){
+      break;
+    }
+    else{
+      $h .= "$tc\;";
+    }
+  }
+  chop($h);
+  print "\>$h\n$seq\n";
+}
+' ${out}/hits.lineage.byseq.tsv > ${out}/hits.lineage.noacc.fa
+
+
+
+rm ${tmp}/${FA}*
 echo "done"
