8d0c2b910b4fd2affa46b6dd4957c5aeb554f2c2
Jonathan Rondeau-Leclaire <jonathan.rondeau-leclaire@usherbrooke.ca>
2026-02-04 08:59:49 -0500

    skip first step when done

--- run_envBarcodeMiner.local.sh
+++ run_envBarcodeMiner.local.sh
@@ -139,7 +139,7 @@ process_index() {
   rm "${tmp}/${FA}"*
 }
 
-if [[ ! -f  ${out}/taxonomy/hits.taxid.tsv ]]; then
+if [[ ! -f ${out}/envBarcodeMiner.results.sqlite ]]; then
     
     TOTAL_INDEX=$(ls ${fa_list}/*.fm9 | wc -l)
     echo "will run dicey search on ${TOTAL_INDEX} indexes using ${threads} threads"
